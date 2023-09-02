b558e2089678c71e720634494e710ea2093e2ad0
CadeMaynard <143521268+CadeMaynard@users.noreply.github.com>
2023-09-01 19:43:15 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,18 @@
 function sum(a) {
-    var sum = a[0];
+    //var sum = a[0];
+    var sum = 0;
     for(var i = 0; i < a.length; i++) {
         sum += a[i];
     }
     return sum;
 }
+
+/*
+Sources:
+https://learning.oreilly.com/library/view/javascript-pocket-reference/9781449335977/ch09.html#id776296
+https://github.com/jsverify/jsverify
+https://www.npmjs.com/package/jsverify?activeTab=readme
+http://www.java2s.com/ref/javascript/javascript-array-forall-func-context.html
+https://stackoverflow.com/questions/15313418/what-is-assert-in-javascript
+The book: Javascript & AJAX for Dummies
+*/
